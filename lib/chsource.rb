22cd6e2dab2768722c3348fe222a03051c5b6aff
Hao Wei <imwillmouse@gmail.com>
2013-08-22 16:04:56 +0800

    v0.0.2

--- lib/chsource.rb
+++ lib/chsource.rb
@@ -1,7 +1,3 @@
 require "chsource/version"
 require 'chsource/chsource'
 require 'chsource/cli'
-
-module Chsource
-  # Your code goes here...
-end
--- lib/chsource/chsource.rb
+++ lib/chsource/chsource.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 module Chsource
   class Chsource
     Source = {
